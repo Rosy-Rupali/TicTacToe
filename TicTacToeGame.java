cc39e0867f1f747972c7cce95a369ac61265d39c
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-04 09:08:38 +0530

    refactored the code

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -4,19 +4,16 @@ package tictactoe;
 
 public class TicTacToeGame {
 
-	public static void main(String args[]) {
-		System.out.println("TicTacToe game board");
-	}
-
+	static char[][] board = new char[3][3];
 	
 	public static void UC1() {
-		char[][] board = new char[3][3];
-		
+		System.out.println("TicTacToe game board");
 		for(int i=1; i<=board.length; i++) {
 			for(int j=1; j<=board[i].length; j++) {
-				board[i][j]= " ";
+				board[i][j]= '-';
 			}
 		}
 		
 	}
+	
 }
