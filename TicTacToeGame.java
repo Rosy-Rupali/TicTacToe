56e932591ac653760a794fddecdb48a100508f92
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-05 08:34:28 +0530

    added: choose letter the UC2

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -16,9 +16,27 @@ public class TicTacToeGame {
 				board[i]= ' ';
 			}
 		}
+	
+	/**
+	 * @taking input from user to choose the turn
+	 */
+	static void chooseLetter() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the character X or O to choose the turn:");
+		char ch = sc.next().charAt(0);
+		if(ch == 'X' || ch == 'x') {
+			System.out.println("player's turn to play the game");
+		}else if(ch == 'O' || ch == 'o') {
+			System.out.println("computer's turn to play the game");
+		}else{
+			System.out.println("Invalid input");
+		}
+			
+	}
 public static void main(String args[]) {
 		
 		createBoard();
+		chooseLetter();
 		
 	}
 }		
