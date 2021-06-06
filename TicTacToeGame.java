165754496d7346ff74fc7fc70b2094de9a4e9aa7
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-06 23:07:52 +0530

    added: Computer check to play win move

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -27,10 +27,10 @@ public class TicTacToeGame {
 		System.out.println("Enter the character X or O to choose the turn:");
 		char turn = sc.next().charAt(0);
 		if (turn == 'X' || turn == 'x') {
-			System.out.println("player's turn to play the game");
+			System.out.println("Computer's turn to play the game");
 			turn = 'O';
 		} else if (turn == 'O' || turn == 'o') {
-			System.out.println("computer's turn to play the game");
+			System.out.println("Player's turn to play the game");
 			turn = 'X';
 		} else {
 			System.out.println("Invalid input");
@@ -138,6 +138,10 @@ public class TicTacToeGame {
 		System.out.println(turn + "turn; enter a slot number to place the " + turn + " in: ");
 		return null;
 
+	}
+	
+	private static void computerCheckToWin() {
+		board[turn] = 'X';
 	}
 
 	public static void main(String args[]) {
